23634e66a172dbcfd0bcc424633b90597c02d9ce
Farhan Masood <farhan.masood@mslm.io>
2024-04-01 14:03:49 +0500

    final zoom clone

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(
+    now,
+  );
+
   return (
-    <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+    <section className="flex size-full flex-col gap-5 text-white">
+      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full justify-center items-center max-md:px-5 max-md:py-8 lg:p-11">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl text-center">
+              {time}
+            </h1>
+            <p className="text-3xl font-medium text-sky-1 lg:text-4xl text-center">
+              {date}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <MeetingTypeList />
     </section>
   );
 };
